06af1cecad4d70098e74ce0a2d3a3342cacf8fb2
muhammadammar24 <muhammadammar2492@gmail.com>
2024-12-06 16:27:49 +0500

    integrate api

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Lato } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Navbar from "@/components/Navbar";
+import { Toaster } from "@/components/ui/toaster";
 
 const lato = Lato({
   subsets: ["latin"],
@@ -29,6 +30,7 @@ export default function RootLayout({
           <div className="max-w-6xl mx-auto min-h-screen">
             <Navbar />
             {children}
+            <Toaster />
           </div>
         </body>
     </html>
